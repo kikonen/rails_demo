efff6a33b23b48a1b9acb4fe54a92058074fbcde
kari <mr.kari.ikonen@gmail.com>
2013-05-17 11:53:01 +0300

    BUILD: Added microposts - NEW: micropost - FIX: model dependencies - FIX: email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
+  has_many :microposts
+
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create }
 end
